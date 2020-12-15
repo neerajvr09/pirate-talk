7ae8c610fc7450d45708e73602ed3487f15ef257
neerajvr09 <neeraj.vr09@gmail.com>
2020-12-15 14:07:43 +0530

    done
    Added servel url.

---
+++ javascript/app.js
@@ -0,0 +1,39 @@
+// getting textarea reference
+var txtInput = document.querySelector(".inputText");
+//getting button reference
+var translateBtn = document.querySelector(".translate-btn");
+// getting outputdiv reference
+var txtOutput = document.querySelector(".outputText");
+
+//server url
+var serverUrl ="https://api.funtranslations.com/translate/pirate.json"
+
+function getTranslationUrl(text){
+    return serverUrl + "?" +"text=" + text
+}
+function errorHandler(error){
+    alert("something wrong with sserver, try again after sometimes")
+}
+function clickHandler(){
+    var inputTxt = txtInput.value // taking input
+
+    //proccessing
+    fetch(getTranslationUrl(inputTxt))
+    .then(response => response.json())
+    .then(json => txtOutput.innerText = json.contents.translated)
+    .catch(errorHandler)
+}
+
+
+// // adding event listner to button
+var translateBtnListner = translateBtn.addEventListener("click", clickHandler);
+
+
+
+
+
+
+
+
+
+
